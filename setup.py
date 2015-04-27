e02d721a97d70fc43aa786f5138476242a9bfb7d
Thomas Thoren <thomasjthoren@gmail.com>
2015-04-27 17:17:30 -0500

    Trying again with setup.py

--- setup.py
+++ setup.py
@@ -1,26 +1,27 @@
 # -*- coding: utf-8 -*-
-from __future__ import unicode_literals
+
 from setuptools import setup
 
 
 setup(
     name='land-records',
     version='0.0.1',
-    packages=[
+    packages=(
         'landrecords',
         'tests',
-    ],
+    ),
     include_package_data=True,
     license='MIT',
     description=(
         "A package for scraping and publishing New Orleans land " +
         "records."),
+    long_description="",
     keywords="The Lens land records",
     url='http://vault.thelensnola.org/realestate/',
     author='Thomas Thoren',
     author_email='tthoren@thelensnola.org',
     install_requires=(
         'Flask>=0.10.1',
-        'SQLAlchemy==0.9.9'
+        'SQLAlchemy'
     ),
 )
